e30d3ecb6da446b2c2d676c8e8d2c144015ae91f
saileela <saileela.user@gmail.com>
2024-01-02 19:47:13 +0530

    dfdffd

--- dynamo.js
+++ dynamo.js
@@ -1,7 +1,10 @@
 const AWS = require('aws-sdk');
 
-// Set the region for your DynamoDB table
-AWS.config.update({ region: 'your-region' });
+AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: 'us-east-1'
+  });
 
 // Create a DynamoDB DocumentClient
 const docClient = new AWS.DynamoDB.DocumentClient();
